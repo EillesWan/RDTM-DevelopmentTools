2eeb0e6da9a47126e017d79a6c31835bd787a662
ZiDui <ziduiminecraft@outlook.com>
2022-07-30 10:23:29 +0000

    删除文件 生成简单的对话字幕/ZiDuiVersion/Tpack/BatchGenerateSubtitle.py

--- 生成简单的对话字幕/ZiDuiVersion/Tpack/BatchGenerateSubtitle.py
+++
@@ -1,7 +0,0 @@
-def BatchSubtitle(Selector,Scoreboard_name,Scoreboard_scores,StrS):
-    Sopenmcfunction=open('AutoT.mcfunction','w')
-    Sopenmcfunction.write('/execute @a' + Selector + " ~ ~ ~ " + "/execute @s[scores={" + Scoreboard_name + "=" + str(
-        Scoreboard_scores) + "}] ~ ~ ~ " + "/title @s subtitle " + StrS)
-    print('/execute @a' + Selector + " ~ ~ ~ " + "/execute @s[scores={" + Scoreboard_name + "=" + str(
-        Scoreboard_scores) + "}] ~ ~ ~ " + "/title @s subtitle " + StrS)
-    Sopenmcfunction.close()
