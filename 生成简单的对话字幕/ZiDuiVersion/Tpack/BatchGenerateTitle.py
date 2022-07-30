8a798272839c8e35f093d916cdaa53296061bb7b
ZiDui <ziduiminecraft@outlook.com>
2022-07-30 10:23:35 +0000

    删除文件 生成简单的对话字幕/ZiDuiVersion/Tpack/BatchGenerateTitle.py

--- 生成简单的对话字幕/ZiDuiVersion/Tpack/BatchGenerateTitle.py
+++
@@ -1,7 +0,0 @@
-def BatchTitle(Selector,Scoreboard_name,Scoreboard_scores,StrT):
-    Topenmcfunction=open('AutoT.mcfunction','w')
-    Topenmcfunction.write('/execute @a' + Selector + " ~ ~ ~ " + "/execute @s[scores={" + Scoreboard_name + "=" + str(
-        Scoreboard_scores) + "}] ~ ~ ~ " + "/title @s title " + StrT)
-    print('/execute @a' + Selector + " ~ ~ ~ " + "/execute @s[scores={" + Scoreboard_name + "=" + str(
-        Scoreboard_scores) + "}] ~ ~ ~ " + "/title @s title " + StrT)
-    Topenmcfunction.close()
